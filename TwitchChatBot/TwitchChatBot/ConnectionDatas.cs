d81c4843182acf9a02c55ff7e33fddd0c14d8ead
DzsepettoConti <159457705+DzsepettoConti@users.noreply.github.com>
2024-12-29 20:28:16 +0100

    first push

---
+++ TwitchChatBot/TwitchChatBot/ConnectionDatas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Api;
+using TwitchLib.Client.Models;
+
+namespace TwitchChatBot
+{
+    internal class ConnectionDatas
+    {
+        public ConnectionCredentials Credentials;
+        public TwitchAPI API;
+
+        public string BotUsername = "OldMonk_Bot", BotOAuth = "0ab906dars0abxim8vxyxarnob7kk0";
+        public string refreshToken = "sm0xdur5p7njsmxdbu9kv3riba4pf5df29lh16csq45ekzqtir";
+
+        //ezek most az oldMOnkCB cuccai
+        public string clientID = "sr9wohpxzd484uil8jbwfixp8x8jnb";
+        public string secret = "jhquro5zfr1gzvwagf06qmbwyr7oto";
+
+
+        public async void Initialize() 
+        {
+            API = new TwitchAPI();
+            API.Settings.Secret = secret;
+            API.Settings.ClientId = clientID;
+            API.Settings.AccessToken = BotOAuth;
+            await RefreshMyToken();
+        }
+        public async Task RefreshMyToken()
+        {
+            var refreshResult = await API.Auth.RefreshAuthTokenAsync(refreshToken, secret, clientID);
+            BotOAuth = refreshResult.AccessToken;
+            //meg kell még csinálni hogy a refreshelt token bekerüljön a botoAuth helyére <---ez elvileg kész, de elég bot megoldás, minden inditaskor ujrahivja
+
+            //zöld név duplaklikk ha meg akarok tudni róla valamit
+        }
+    }
+}
---
+++ TwitchChatBot/TwitchChatBot/TwitchClientContainer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Api;
+using TwitchLib.Api.Auth;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using System.Speech.Synthesis;
+using TwitchLib.PubSub.Events;
+
+namespace TwitchChatBot
+{
+    internal class TwitchClientContainer
+    {
+
+        public TwitchClient Client;
+        public ConnectionCredentials Credentials;
+        
+        public async void Initialize(string botUser, string botOAuthToken)
+        {
+            Client = new TwitchClient();
+            Credentials = new ConnectionCredentials(botUser, botOAuthToken);
+            Client.OnConnected += OnConnected;
+            Client.OnJoinedChannel += JoinedChannel;
+            Client.OnMessageReceived += MessageRecieved;
+            Client.OnChatCommandReceived += OnChatCommandReceived;
+           // Client.OnLog += OnLog;
+            Client.Initialize(Credentials);
+            Client.Connect();
+        }
+
+        public void SendMessage(string message)
+        {
+            Client.SendMessage(Client.JoinedChannels[0], message);
+        }
+        public void ChatCommand(string trigger, string message, OnChatCommandReceivedArgs e)
+        {
+            if (e.Command.CommandText.Equals(trigger, StringComparison.OrdinalIgnoreCase))
+            {
+                SendMessage(message);
+            }
+        }
+        public void TTS(string message) 
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;
+            synthesizer.Rate = 0;
+
+           
+            synthesizer.Speak(message);
+            Console.WriteLine("vége a felolvasásnak");
+        }
+        public void TTSMessage(OnChatCommandReceivedArgs e)
+        {
+            
+            string fullMessage = e.Command.ChatMessage.Message;
+
+            
+            if (fullMessage.Length > "!hang ".Length)
+            {
+                string hangText = fullMessage.Substring("!hang ".Length).Trim();
+                TTS(hangText);
+                Console.WriteLine($"A hang parancs tartalma: {hangText}");
+            }
+            else
+            {
+                Console.WriteLine("Nem adtál meg szöveget a !hang parancs után.");
+            }
+        }
+
+        private void OnChatCommandReceived(object? sender, OnChatCommandReceivedArgs e)
+        {
+            ChatCommand("beep", "boop", e);
+            ChatCommand("pisi", "kaka", e);
+
+            if (e.Command.CommandText.Equals("hang", StringComparison.OrdinalIgnoreCase))
+            {
+                TTSMessage(e);
+            }
+
+            
+
+        }
+        
+        private void OnLog(object? sender, TwitchLib.Client.Events.OnLogArgs e)
+        {
+            Console.WriteLine($"Log: {e.Data}");
+        }
+
+        private void OnConnected(object? sender, TwitchLib.Client.Events.OnConnectedArgs e)
+        {
+            Console.WriteLine($"I Have Connected");
+            Client.JoinChannel("geppo2tv");
+        }
+        private void JoinedChannel(object? sender, TwitchLib.Client.Events.OnJoinedChannelArgs e)
+        {
+            Console.WriteLine($"I have connected to the channel named: {e.Channel}.");
+            //SendMessage("Itten vagyok");
+        }
+        private void MessageRecieved(object? sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+        {
+            Console.WriteLine($"Message from {e.ChatMessage.Username} : {e.ChatMessage.Message} ");
+        }
+
+    }
+}
